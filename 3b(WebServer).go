210b222eae298049fe8a46aa20072481c500c65d
Soniya Kambli <100508652+techsoniya@users.noreply.github.com>
2023-07-05 16:34:38 +0000

    update

---
+++ 3b(WebServer).go
@@ -0,0 +1,31 @@
+//Web Server
+
+package main
+import (
+	"fmt"
+ 	"net/http"
+	"log"
+)
+
+func main(){
+	//Define the route handler function
+	handler := func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w,"Hello World!!")
+	}
+
+	//Cretae a new server mux
+	mux := http.NewServeMux()
+
+	//Register the route handler function to handle all requests to "/"
+	mux.HandleFunc("/",handler)
+
+	//Create a new HTTP server
+	server := http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	//Start the HTTP server
+	log.Println("Server listening on port 8080....")
+	log.Fatal(server.ListenAndServe())
+}
